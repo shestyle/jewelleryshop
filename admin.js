7ba601fb5bc7aa8fd6b1dc241a528c30386e3062
shestyle <50356311+shestyle@users.noreply.github.com>
2026-01-02 17:40:34 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,31 +1,36 @@
-import { auth, db, storage } from "./firebase.js";
-import { signInWithEmailAndPassword } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { db, storage } from "./firebase.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { ref, uploadBytes, getDownloadURL } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-import { addDoc, collection } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-window.login = () => {
-  signInWithEmailAndPassword(
-    auth,
-    email.value,
-    password.value
-  ).then(() => alert("Logged in"));
-};
+const list=document.getElementById("adminList");
 
-window.uploadProduct = async () => {
-  const file = image.files[0];
-  const imgRef = ref(storage, `jewelry/${Date.now()}`);
-  await uploadBytes(imgRef, file);
-  const url = await getDownloadURL(imgRef);
+window.upload=async()=>{
+  const file=image.files[0];
+  const refImg=ref(storage,Date.now()+"");
+  await uploadBytes(refImg,file);
+  const url=await getDownloadURL(refImg);
 
   await addDoc(collection(db,"products"),{
-    name: name.value,
-    price: price.value,
-    category: category.value,
-    imageURL: url
+    name:name.value,
+    price:price.value,
+    imageURL:url
   });
-
-  alert("Product uploaded");
+  load();
 };
+
+async function load(){
+  list.innerHTML="";
+  const snap=await getDocs(collection(db,"products"));
+  snap.forEach(d=>{
+    list.innerHTML+=`
+      <div>
+        ${d.data().name}
+        <button onclick="del('${d.id}')">❌</button>
+      </div>`;
+  });
+}
+
+window.del=id=>deleteDoc(doc(db,"products",id)).then(load);
+load();
